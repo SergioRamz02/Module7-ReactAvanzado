9e6143ed2f30305cdcbb3d71d8adb282f5cdf1d2
Sergio Ramírez <sergio_esfm02@hotmail.com>
2025-07-10 21:33:53 -0600

    feat: Clase 2, practica uso de fetch y API

--- mi-blog/src/Components/ArticulosBlog.jsx
+++ mi-blog/src/Components/ArticulosBlog.jsx
@@ -2,17 +2,17 @@ import React, {useState, useEffect} from "react";
 import "../assets/Styles/ArticulosBlog.css"
 
 export default function ArticulosBlog(){
-    const [articulos, setArticulos] = useState([]);
+    const [cartas, setCartas] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState(null);
 
     useEffect(()=>{
-        fetch('https://jsonplaceholder.typicode.com/posts?_limit=5')
+        fetch('https://db.ygoprodeck.com/api/v7/cardinfo.php')
         .then(res =>{
-            if (!res.ok) throw new Error("Error al obtener los articulos");
+            if (!res.ok) throw new Error("Error al obtener las cartas de YU-GI-OH");
             return res.json();
         })
-        .then(data => setArticulos(data))
+        .then(data => setCartas(data.data))
         .catch(err => setError(err))
         .finally(()=> setIsLoading(false));
     }, []);
@@ -22,11 +22,15 @@ export default function ArticulosBlog(){
 
     return (
         <div className="articulos">
-            <h2>Articulos de mi blog</h2>
-            {articulos.map(articulo => (
-                <div className="card" key={articulo.id}>
-                    <h3>{articulo.title}</h3>
-                    <p>{articulo.body}</p>
+            <h2>Cartas de YU-GI-OH!</h2>
+            {cartas.map(carta => (
+                <div className="card" key={carta.id}>
+                    <h3>{carta.name}</h3>
+                    <p>{carta.type}</p>
+                    <p>{carta.race}</p>
+                    <p>{carta.desc}</p>
+                    <img src={carta.card_images[0].image_url_small} alt={carta.name} />
+                
                 </div>
             ))}
         </div>
