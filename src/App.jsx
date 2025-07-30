28d4151058a50afe02bd87ffae1054d3b61de12a
Sergio Ramírez <sergio_esfm02@hotmail.com>
2025-07-29 19:27:01 -0600

    feat: Se envuelve el app en main y se unifica la app para solo utilizar PostList como mi todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,9 @@
-import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Login from './pages/Login'
 import PostList from './pages/PostList'
-import Register from './pages/Register'
-import CreatePost from './pages/CreatePost'
+
 
 function App() {
 
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<PostList />}></Route>
-        <Route path='/login' element={<Login />}></Route>
-        <Route path='/register' element={<Register />}></Route>
-        <Route path='/create' element={<CreatePost />}></Route>
-      </Routes>
-    </BrowserRouter>
-  )
+  return <PostList />
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { UserProvider } from './Context/UserContext.jsx'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <UserProvider>
+       <App />
+    </UserProvider>
   </StrictMode>,
 )
